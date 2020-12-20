2ef45492e7f39a50eb1d5cab1be051e84f09a0ab
aleksasln <sasha2016912@gmail.com>
2020-12-20 21:29:12 +0500

    Создаем функцию для выдачи рандомных бочек

--- code.py
+++ code.py
@@ -1,4 +1,11 @@
 from tkinter import *
 import random
 N = int(input("Сколько маленьких бочек должно быть в мешке: "))
-spam = list(range(1, N+1))
+spam = list(range(1, N+1)) #Создаем список
+def random_number(): #Функция для выдачи рандомных номеров бочек
+    if  len(spam)>1 :
+        number.set(random.choice(spam))
+        a=random.choice(spam)
+        spam.remove(a)
+    else:
+        number.set(spam)
